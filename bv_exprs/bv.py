381416efd8bb0e195480257721f606c8b3b28d51
Yi Zhou <yizhou7@outlook.com>
2020-07-27 17:04:57 -0400

    int2bv2int_test gets sluggish at 32 bits

--- bv_exprs/bv.py
+++ bv_exprs/bv.py
@@ -47,9 +47,8 @@ def int2bv2int_test(bits):
     mask = half - 1
 
     bvx = BitVec('x', bits)
-    prove(
-        bvx == LShR(bvx, shift) * half + (bvx & mask)
-    )
+    query = (bvx == LShR(bvx, shift) * half + (bvx & mask))
+    prove(query)
 
     x = Int('x')
     bvx = Int2BV(x, bits)
@@ -65,30 +64,8 @@ def int2bv2int_test(bits):
                 And(
                     0 <= x, x < full,
                 ),
-                x == BV2Int(LShR(bvx, shift) * half + BV2Int(bvx & mask)
+                x == BV2Int(LShR(bvx, shift)) * half + BV2Int(bvx & mask)
             )
     prove(query)
 
-    # x = Int('x')
-    # y = Int('y')
-    # z = Int('z')
-    # lh = Int('lh')
-    # uh = Int('uh')
-
-    # query = Implies(
-    #             And(
-    #                 0 <= x, x < half,
-    #                 0 <= y, y < half,
-    #                 0 <= z, z < full,
-
-    #                 z == x * y,
-
-    #                 lh == BV2Int(Int2BV(z, bits) & (half - 1)),
-    #                 uh == BV2Int(LShR(Int2BV(z, bits), bits / 2)),
-    #             ),
-    #             uh * half + lh == x * y,
-    #         )
-
-# pure_bv_test()
-# bv2int_test()
-int2bv2int_test(256)
+int2bv2int_test(32)
