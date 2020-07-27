9132f267b410533784ef9e94761c3190da4d79e9
Yi Zhou <yizhou7@outlook.com>
2020-07-27 14:37:16 -0400

    update bv tests

--- bv_exprs/bv.py
+++ bv_exprs/bv.py
@@ -1,66 +1,66 @@
 from z3 import *
 
-x = BitVec('x', 256)
-y = BitVec('y', 256)
-z = BitVec('z', 256)
-lh = BitVec('lh', 256)
-uh = BitVec('uh', 256)
 
-prove(
-    x == LShR(x, 128) * 340282366920938463463374607431768211456 + (x & 340282366920938463463374607431768211455)
-)
+def pure_bv_test():
+    x = BitVec('x', 256)
+    y = BitVec('y', 256)
+    z = BitVec('z', 256)
+    lh = BitVec('lh', 256)
+    uh = BitVec('uh', 256)
 
-query = Implies(
-            And(
-                x == y * z,
-                lh == x & 340282366920938463463374607431768211455,
-                uh == LShR(x, 128),
-            ),
-            uh * 340282366920938463463374607431768211456 + lh == y * z,
-        )
-
-prove(query)
-
-query = Implies(
-            And(
-                x == y * z,
-                lh == x & 340282366920938463463374607431768211455,
-                uh == LShR(x, 128),
-            ),
-            BV2Int(uh * 340282366920938463463374607431768211456 + lh) == BV2Int(y * z),
-        )
-
-prove(query)
-
-x = BitVec('x', 32)
-y = BitVec('y', 32)
-
-prove(
-    Implies(
-        y == (LShR(x, 16)),
-        y <= 0xffff,
+    prove(
+        x == LShR(x, 128) * 340282366920938463463374607431768211456 + (x & 340282366920938463463374607431768211455)
     )
-)
 
-prove(
-    Implies(
-        y == (LShR(x, 16)),
-        BV2Int(y, is_signed=False) <= 0xffff,
-    )
-)
+    query = Implies(
+                And(
+                    x == y * z,
+                    lh == x & 340282366920938463463374607431768211455,
+                    uh == LShR(x, 128),
+                ),
+                uh * 340282366920938463463374607431768211456 + lh == y * z,
+            )
 
+    prove(query)
 
+def bv2int_test():
+    x = BitVec('x', 256)
+    y = BitVec('y', 256)
+    z = BitVec('z', 256)
+    lh = BitVec('lh', 256)
+    uh = BitVec('uh', 256)
 
+    query = Implies(
+                And(
+                    x == y * z,
+                    lh == x & 340282366920938463463374607431768211455,
+                    uh == LShR(x, 128),
+                ),
+                BV2Int(uh * 340282366920938463463374607431768211456 + lh) == BV2Int(y * z),
+            )
+    prove(query)
 
-# x = Int('x')
-# y = Int('y')
-# prove(
-#     Implies(
-#         And(
-#             x < 115792089237316195423570985008687907853269984665640564039457584007913129639936,
-#             x >= 0,
-#             y == BV2Int(Int2BV(x, 256) >> 128),
-#         ),
-#         y < 340282366920938463463374607431768211456,
-#     )
-# )
+def int2bv2int_test():
+    x = Int('x')
+    y = Int('y')
+    z = Int('z')
+    lh = Int('lh')
+    uh = Int('uh')
+
+    query = Implies(
+                And(
+                    0 <= y,
+                    y < 340282366920938463463374607431768211456,
+                    0 <= z,
+                    z < 340282366920938463463374607431768211456,
+                    x == y * z,
+                    lh == BV2Int(Int2BV(x, 256) & 340282366920938463463374607431768211455),
+                    uh == BV2Int(LShR(Int2BV(x, 256), 128)),
+                ),
+                uh * 340282366920938463463374607431768211456 + lh == y * z,
+            )
+    prove(query)
+
+pure_bv_test()
+bv2int_test()
+int2bv2int_test()
