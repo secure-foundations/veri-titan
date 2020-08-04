fb0748cc01d1c9b910be99d7d283d46673bb635c
Yi Zhou <yizhou7@outlook.com>
2020-08-04 11:42:32 -0400

    suprprusingly the second lemma scales better

--- otbn_impl/d0inv.py
+++ otbn_impl/d0inv.py
@@ -1,8 +1,9 @@
-num_bits = 32
+num_bits = 64
 BASE = 2 ** num_bits
 
 def print_bin(num):
     print(bin(num)[2:][-num_bits:].zfill(num_bits))
+
 
 def d0inv(w28):
     w0 = 2
@@ -11,28 +12,30 @@ def d0inv(w28):
     for i in range(1, num_bits):
 
         x = w29 * w28
-        q = x // 2 ** i
 
         w1 = (w28 * w29) % BASE
         w1 = w1 & w0
 
-        assert ((w1 == 0) == (q % 2 == 0))
+        # q = x // 2 ** i
+        # assert ((w1 == 0) == (q % 2 == 0))
 
-        if q % 2 == 0:
+        if w1 == 0:
             # print_bin(x)
-            # print_bin(q)
-            # print_bin(2 ** i)
-            # print_bin(2 ** (i + 1))
+            # print_bin(1 << i)
+            # print_bin(1 << (i + 1))
+
+            assert(x % (1 << i) == 1)
+            # ==> 
+            assert(x % (1 << (i + 1)) == 1)
+        else:
+            # print_bin(x)
+            # print_bin(1 << i)
+            # # print_bin(1 << (i + 1))
             # print()
 
-            assert(x % 2 ** i == 1)
-            # ==> 
-            assert(x % 2 ** (i + 1) == 1)
-        else:
-            assert(x % 2 ** i == 1)
+            assert(x % (1 << i) == 1)
             # ==>
-            assert((x + w28 * 2 ** i) % 2 ** (i + 1) == 1)
-
+            assert((x + w28 * (1 << i)) % (1 << (i + 1)) == 1)
 
         w29 = w29 | w1
         w0 = w0 * 2
@@ -44,7 +47,6 @@ d0inv(2109612375)
 
 from z3 import *
 
-# w1 = BitVec("w1", num_bits)
 x = BitVec("x", num_bits)
 i = BitVec("i", num_bits)
 
@@ -59,7 +61,17 @@ query = Implies(
 )
 prove(query)
 
-# i = BitVecVal(30, 32)
-# print(simplify(1 << (i + 1)))
-x = BitVecVal(2147483649, 32)
-print(simplify(x % 2147483648))
+w28 = BitVec("w28", num_bits)
+
+query = Implies(
+    And(
+        0 <= i,
+        i < num_bits,
+        URem(x, (1 << i)) == 1,
+        x & (1 << i) == 1,
+        URem(w28, 2) == 1,
+    ),
+    URem(x + w28 * (1 << i), (1 << (i + 1))) == 1,
+)
+prove(query)
+
