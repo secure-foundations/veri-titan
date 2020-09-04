1c1079cdf564cac70df3fb4ca7fe2dc712bab5f5
Yi Zhou <yizhou7@outlook.com>
2020-09-04 00:06:57 -0400

    not sure whats wrong with the implication

--- bv_exprs/bv_export.py
+++ bv_exprs/bv_export.py
@@ -1,17 +1,22 @@
 import os
 import pyboolector
-from pyboolector import Boolector, BoolectorException, BTOR_OPT_PRINT_DIMACS, BTOR_OPT_MODEL_GEN
+from pyboolector import Boolector, BoolectorException
+from pyboolector import BTOR_OPT_PRINT_DIMACS, BTOR_OPT_MODEL_GEN
 
 import sys
 
 try:
 	full_bits = int(sys.argv[2])
 except:
-	full_bits = 3
+	full_bits = 2
 
-def prove(x):
+def prove(btor, x):
 	btor.Assert(btor.Not(x))
-	return btor.Sat()
+	return btor.Sat() == Boolector.UNSAT
+
+def check(btor, x):
+	btor.Assert(x)
+	return btor.Sat() == Boolector.SAT
 
 half_bits = int(full_bits / 2)
 BASE = 2 ** full_bits
@@ -22,19 +27,17 @@ btor = Boolector()
 full_sort = btor.BitVecSort(full_bits)
 half_sort = btor.BitVecSort(half_bits)
 
+# btor.Set_opt(BTOR_OPT_EXIT_CODES, 1)
 # btor.Set_opt(BTOR_OPT_MODEL_GEN, 1)
-btor.Set_opt(BTOR_OPT_PRINT_DIMACS, 1)
+# btor.Set_opt(BTOR_OPT_PRINT_DIMACS, 1)
 
 x = btor.Var(full_sort, "x")
 y = btor.Var(full_sort, "y")
 z = btor.Var(full_sort, "z")
 
 q = btor.Implies(
-	x * z == y * z,
+	x & z == y & z,
 	x == y)
-
-print(prove(q))
-# print(x.assignment)
-# print(y.assignment)
-# print("{} {}".format(x.symbol, x.assignment))  # prints: x 00000100
-# print("{} {}".format(y.symbol, y.assignment))  # prints: y 00010101
+# q = x != x
+print(prove(btor, q))
+# print(check(btor, q))
