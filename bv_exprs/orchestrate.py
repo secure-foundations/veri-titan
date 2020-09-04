b8d2094d660031a725a46f12c9afafdc4d9f926d
Yi Zhou <yizhou7@outlook.com>
2020-09-04 14:11:40 -0400

    builds a graph

--- bv_exprs/orchestrate.py
+++ bv_exprs/orchestrate.py
@@ -5,20 +5,35 @@ try:
 except:
 	full_bits = 2
 
+picosat = "/home/yizhou7/Desktop/research/open_titan/picosat-965/picosat"
+
 print("using %d bit width" % full_bits)
 
-# /home/yizhou7/Desktop/research/open_titan/picosat-965/picosat
-os.system("python bv_export.py %d | tail -n +4 | head -n -1", % full_bits)
+if not os.path.exists("gen"):
+	os.makedirs("gen")
 
-# f = open(sys.argv[1])
-# content = "digraph D {\n"
-# for line in f:
-# 	if "*" not in line:
-# 		continue
-# 	line = line.strip().split()[:-1]
-# 	start = line[0]
-# 	for end in line[2:]:
-# 		content += "\t{} -> {}\n".format(start, end)
-# content += "}\n"
+name = "gen/%d_bits" % full_bits
+cnf_file = name + ".cnf"
+trace_file = name + ".trace"
+dot_file = name + ".dot"
+png_file = name + ".png"
 
-# print(content)
+os.system("python bv_export.py %d | tail -n +4 | head -n -1 > %s" % (full_bits, cnf_file))
+os.system("%s %s -t %s" % (picosat, cnf_file, trace_file))
+
+f = open(trace_file)
+content = "digraph D {\n"
+for line in f:
+	if "*" not in line:
+		continue
+	line = line.strip().split()[:-1]
+	start = line[0]
+	for end in line[2:]:
+		content += "\t{} -> {}\n".format(start, end)
+content += "}\n"
+
+cg_out = open(dot_file, "w+")
+cg_out.write(content)
+cg_out.close()
+
+os.system("dot -Tpng %s -o %s" % (dot_file, png_file))
