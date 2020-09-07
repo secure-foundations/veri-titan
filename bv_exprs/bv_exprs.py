bcb9173c42e5c8879dd9dbeff8982c557325663f
Yi Zhou <yizhou7@outlook.com>
2020-09-07 19:55:37 -0400

    export smt query

--- bv_exprs/bv_exprs.py
+++ bv_exprs/bv_exprs.py
@@ -15,8 +15,7 @@ HALF_BASE = int(2 ** half_bits)
 def bvand():
 	x = BitVec("x", full_bits)
 	y = BitVec("y", full_bits)
-	query = x & y == y & x
-	prove(query)
+	return x & y == y & x
 
 def shift():
 	amt = BitVec("amt", full_bits)
@@ -26,38 +25,32 @@ def shift():
 		),
 		UGT(1 << amt, 0),
 	)
-	prove(query)
+	return query
 
 def xor():
 	x = BitVec("x", full_bits)
 	y = BitVec("y", full_bits)
 	z = BitVec("z", full_bits)
 
-	query = (
-		Implies(
-			x ^ z == y ^ z,
-			x == y,
-		)
+	return Implies(
+		x ^ z == y ^ z,
+		x == y,
 	)
-	prove(query)
 
 def andorxor_135():
 	x = BitVec("x", full_bits)
 	c1 = BitVec("c1", full_bits)
 	c2 = BitVec("c2", full_bits)
-	query = (x & c2) ^ (c1 & c2) == (x ^ c1) & c2
-	prove(query)
+	return (x & c2) ^ (c1 & c2) == (x ^ c1) & c2
 
 def addsub_1043():
 	x = BitVec("x", full_bits)
 	y = BitVec("y", full_bits)
-	query = 0 == ((x & y) ^ y) + 1 + (x | ~y)
-	prove(query)
+	return 0 == ((x & y) ^ y) + 1 + (x | ~y)
 
 def addsub_1156():
 	x = BitVec("x", full_bits)
-	query = (x + x == (x << 1))
-	prove(query)
+	return x + x == (x << 1)
 
 # doesn't scale so well
 def mul():
@@ -65,19 +58,16 @@ def mul():
 	x = BitVec("x", full_bits)
 	y = BitVec("y", full_bits)
 
-	query = (
-		Implies(
-			And(
-				0 <= x, x < HALF_BASE,
-				0 <= y, y < HALF_BASE,
-				0 <= m, m < HALF_BASE,
-				m != 0,
-				x * m == y * m,
-			),
-			x == y,
-		)
+	return Implies(
+		And(
+			0 <= x, x < HALF_BASE,
+			0 <= y, y < HALF_BASE,
+			0 <= m, m < HALF_BASE,
+			m != 0,
+			x * m == y * m,
+		),
+		x == y,
 	)
-	prove(query)
 
 def mul_reasm():
 	x = BitVec('x', full_bits)
@@ -99,24 +89,21 @@ def mul_reasm():
 	# yhi : ylo == y
 	# x * y == uh(xlo, ylo) + xhi * ylo + xlo * yhi : xlo * ylo
 
-	query = (
-		Implies(
-			And(
-				Extract(full_bits-1, half_bits, x) == xhi,
-				Extract(half_bits-1, 0, x) == xlo,
-				Extract(full_bits-1, half_bits, y) == yhi,
-				Extract(half_bits-1, 0, y) == ylo,
-			),
-			Concat(mulhu(xlo, ylo, half_bits) + xhi * ylo + xlo * yhi, xlo * ylo) == x * y
-		)
+	return Implies(
+		And(
+			Extract(full_bits-1, half_bits, x) == xhi,
+			Extract(half_bits-1, 0, x) == xlo,
+			Extract(full_bits-1, half_bits, y) == yhi,
+			Extract(half_bits-1, 0, y) == ylo,
+		),
+		Concat(mulhu(xlo, ylo, half_bits) + xhi * ylo + xlo * yhi, xlo * ylo) == x * y
 	)
-	prove(query)
 
 def d0inv_1():
 	x = BitVec("x", full_bits)
 	i = BitVec("i", full_bits)
 
-	query = Implies(
+	return Implies(
 		And(
 			0 <= i,
 			i < full_bits,
@@ -127,14 +114,13 @@ def d0inv_1():
 		URem(x, (1 << (i + 1))) == 1,
 		# x & ((1 << (i + 1)) - 1) == 1,
 	)
-	prove(query)
 
 def d0inv_2():
 	x = BitVec("x", full_bits)
 	w28 = BitVec("x", full_bits)
 	i = BitVec("i", full_bits)
 
-	query = Implies(
+	return Implies(
 		And(
 			0 <= i,
 			i < full_bits,
@@ -146,10 +132,18 @@ def d0inv_2():
 		URem(x + w28 * (1 << i), (1 << (i + 1))) == 1,
 		# (x + (w28 << i)) & (1 << (i + 1)) - 1 == 1,
 	)
-	prove(query)
 
 try:
-	a = sys.argv[1] + "()"
-	eval(a)
+	func = sys.argv[1] + "()"
+	query = eval(func)
+	prove(query)
+
+	s = Solver()
+	s.push()
+	s.add(Not(query))
+	print(s.sexpr(), end="")
+	print("(check-sat)")
+
 except:
 	print("usage:\npython bv_exprs.py [function_name] [bit_number]")
+	sys.exit(1)
