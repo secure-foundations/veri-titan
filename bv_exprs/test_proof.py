5423eb19836b372923ac0a5fff92fad405c65168
Yi Zhou <yizhou7@outlook.com>
2020-09-07 20:05:30 -0400

    rename to test_proof

--- bv_exprs/test_proof.py
+++ bv_exprs/test_proof.py
@@ -19,27 +19,29 @@ trace_file = name + ".trace"
 dot_file = name + ".dot"
 png_file = name + ".png"
 
-print("\nexporting smt query")
+print("[1] exporting smt query")
 if os.system("python bv_exprs.py %s %d | tail -n +2 > %s" % (func_name, full_bits, smt_file)) != 0:
 	print("smt export failed")
 	sys.exit(1)
 
-print("running boolector")
+print("[2] running boolector")
 
 if os.system("boolector %s -dd | python filter.py > %s" % (smt_file, cnf_file)) != 0:
 	print("cnf export failed")
 	sys.exit(1)
 
-print("running picosat")
+print("[3] running picosat")
 r = os.system("picosat %s -t %s" % (cnf_file, trace_file))
 
 l = open(cnf_file).readline().strip()
 l = l.split()
 
-print("\nreading trace")
+print("[4] reading trace")
 f = open(trace_file)
 cg_out = open(dot_file, "w+")
 resolution_steps = 0
+
+edge_count = 0
 
 cg_out.write("digraph D {\n")
 for line in f:
@@ -47,19 +49,24 @@ for line in f:
 		continue
 	line = line.strip().split()[:-1]
 	start = line[0]
+	edge_count += len(line[2:])
+	resolution_steps += 1
+
+	if edge_count > 1000:
+		continue
+
 	for end in line[2:]:
 		cg_out.write("\t{} -> {}\n".format(start, end))
-	resolution_steps += 1
-cg_out.write("}\n")
 
+cg_out.write("}\n")
 cg_out.close()
 
-if resolution_steps < 100:
+if edge_count <= 1000:
 	os.system("dot -Tpng %s -o %s" % (dot_file, png_file))
 else:
-	print("too many steps skipping graph generation")
+	print("graph too large skipping visualization")
 
-print("\ndumping stats")
+print("[5] dumping stats")
 print("variables: %s" % l[2])
 print("clauses: %s" % l[3])
 print("proof steps: %d" % resolution_steps)
