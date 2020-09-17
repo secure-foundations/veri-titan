7cd4ac31624db006d0518cdfdc5c475704019b6a
Yi Zhou <yizhou7@outlook.com>
2020-09-17 10:51:37 -0400

    encode xor (wrong?)

--- bv_exprs/convert.py
+++ bv_exprs/convert.py
@@ -4,7 +4,7 @@ class Encoder:
     def __init__(self, q):
         self.tmp_count = 0
         self.bin_count = 0
-        self.input_vars = dict()
+        self.nbits_vars = dict()
         self.opaque_vars = dict()
         self.gpass = False
         self.equations = set()
@@ -45,6 +45,7 @@ class Encoder:
                 if self.gpass:
                     return v + "'"
                 else:
+                    # print("????" + v)
                     self.add_input_var(v)
                     return v
 
@@ -54,42 +55,15 @@ class Encoder:
             if op == "&":
                 self.encode_and(var, children[0], children[1])
                 return var
+            if op == "^":
+                self.encode_xor(var, children[0], children[1])
+                return var
             else:
-                raise Exception("not handled")
+                raise Exception(f"op {op} not handled")
         elif type(e) == z3.z3.BitVecNumRef:
             return str(e)
         else:
             raise Exception("not handled")
-
-    def get_fresh_tmp(self):
-        self.tmp_count += 1
-        return "t%d" % self.tmp_count
-    
-    def get_fresh_bin(self):
-        self.bin_count += 1
-        b = "b%d" % self.bin_count
-        eq = f"{b} * (1 - {b})"
-        self.equations.add(eq)
-        return b
-
-    def add_input_var(self, v):
-        if v not in self.input_vars:
-            b = self.get_fresh_bin()
-            self.input_vars[v] = b
-            eq = f"{v}' - pow2_n_1 * {b} - {v}"
-            self.equations.add(eq)
-
-    def get_fresh_opaque(self, actual):
-        if actual in self.opaque_vars:
-            return self.opaque_vars[actual]
-        o = "o%d" % (len(self.opaque_vars) + 1) 
-        self.opaque_vars[actual] = o
-        return o
-
-    def get_assoc_bin(self, v):
-        if v not in self.input_vars:
-            raise Exception("not an input var")
-        return self.input_vars[v]
 
     def encode_and(self, t, l, r):
         if self.gpass:
@@ -103,7 +77,6 @@ class Encoder:
 
             bl = self.get_assoc_bin(l)
             br = self.get_assoc_bin(r)
-
             b = self.get_fresh_bin()
             eq = f"{b} - {bl} * {br}"
             self.equations.add(eq)
@@ -112,14 +85,69 @@ class Encoder:
             eq = f"{o1} - pow2_n * {b} - {o0}"
             self.equations.add(eq)
 
+    def encode_xor(self, t, l, r):
+        if self.gpass:
+            o0 = self.get_fresh_opaque(f"bv_xor({l}, {r}, n)")
+            eq = f"{t} - {o0}"
+            self.equations.add(eq)
+        else:
+            # -b + bl + br - 2 * bl * br == 0
+            o0 = self.get_fresh_opaque(f"bv_xor({l}, {r}, n - 1)")
+            eq = f"{t} - {o0}"
+            self.equations.add(eq)
+
+            bl = self.get_assoc_bin(l)
+            br = self.get_assoc_bin(r)
+            b = self.get_fresh_bin()
+            eq = f"{bl} + {br} - 2 * {bl} * {br} - {b}"
+            self.equations.add(eq)
+
+            o1 = self.get_fresh_opaque(f"bv_xor({l}', {r}', n)")
+            eq = f"{o1} - pow2_n * {b} - {o0}"
+            self.equations.add(eq)
+
+    def get_fresh_tmp(self):
+        self.tmp_count += 1
+        b = self.get_fresh_bin()
+        t = "t%d" % self.tmp_count
+        self.nbits_vars[t] = b
+        eq = f"{t}' - pow2_n_1 * {b} - {t}"
+        self.equations.add(eq)
+        return t
+
+    def get_fresh_bin(self):
+        self.bin_count += 1
+        b = "b%d" % self.bin_count
+        eq = f"{b} * (1 - {b})"
+        self.equations.add(eq)
+        return b
+
+    def add_input_var(self, v):
+        if v not in self.nbits_vars:
+            b = self.get_fresh_bin()
+            self.nbits_vars[v] = b
+            eq = f"{v}' - pow2_n_1 * {b} - {v}"
+            self.equations.add(eq)
+
+    def get_fresh_opaque(self, actual):
+        if actual in self.opaque_vars:
+            return self.opaque_vars[actual]
+        o = "o%d" % (len(self.opaque_vars) + 1) 
+        self.opaque_vars[actual] = o
+        return o
+
+    def get_assoc_bin(self, v):
+        # print(v)
+        if v not in self.nbits_vars:
+            raise Exception("not an nbits var")
+        return self.nbits_vars[v]
+
     def dump_vars(self):
         vars = ["pow2_n_1", "pow2_n"]
-        for v in self.input_vars:
+        for v in self.nbits_vars:
             vars.append(f"{v}, {v}'")
         for i in range(self.bin_count):
             vars.append(f"b{i + 1}")
-        for i in range(self.tmp_count):
-            vars.append(f"t{i + 1}")
         for o in self.opaque_vars.values():
             vars.append(o)
         print("ring r=integer,(" + ", ".join(vars) + "),lp;")
