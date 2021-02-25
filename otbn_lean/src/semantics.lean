42a640a52971d4acce3e125e3077264585b4dc15
Bryan Parno <parno@cmu.edu>
2021-02-25 17:37:56 -0500

    Get into a "compiling" state

--- otbn_lean/src/semantics.lean
+++ otbn_lean/src/semantics.lean
@@ -1,8 +1,8 @@
 namespace otbn
-set_option pp.beta
+set_option pp.beta true
 
 /-! Basic types -/
-def uint32 := {i : N // i ≤ 0x100000000}
+def uint32 := {i : ℕ // i ≤ 0x100000000}
 
 /-! Register definitions and lemmas adapted from lovelib.lean's definition of state -/
 def registers := nat -> uint32
@@ -12,20 +12,20 @@ def registers := nat -> uint32
          with a set of lemmas that match F*/Dafny's axioms
  -/
 def registers.update (id:nat) (val:uint32) (regs:registers) : registers :=
-  λ id', if id' = id then val else s id'
+  λ id', if id' = id then val else regs id'
 
 notation r `{` id ` ↦ ` val `}` := registers.update id val r
+meta def tactic.dec_trivial := `[exact dec_trivial]
 
-@[simp] lemma update_apply (id : nat) (val : ℕ) (r : registers) :
-  r{id ↦ val} id = val :=
-if_pos rfl
+@[simp] lemma update_apply (id : nat) (val : uint32) (r : registers) :
+  r {id ↦ val} id = val := if_pos rfl
 
-@[simp] lemma update_apply_ne (id id' : nat) (val : ℕ) (r : registers)
+@[simp] lemma update_apply_ne (id id' : nat) (val : uint32) (r : registers)
     (h : id' ≠ id . tactic.dec_trivial) :
-  r{id ↦ val} id' = r id' :=
+  r {id ↦ val} id' = r id' :=
 if_neg h
 
-@[simp] lemma update_override (id : nat) (val₁ val₂ : ℕ) (r : registers) :
+@[simp] lemma update_override (id : nat) (val₁ val₂ : uint32) (r : registers) :
   r{id ↦ val₂}{id ↦ val₁} = r{id ↦ val₁} :=
 begin
   apply funext,
@@ -34,7 +34,7 @@ begin
     simp [h]
 end
 
-@[simp] lemma update_swap (id₁ id₂ : nat) (val₁ val₂ : ℕ) (r : registers)
+@[simp] lemma update_swap (id₁ id₂ : nat) (val₁ val₂ : uint32) (r : registers)
     (h : id₁ ≠ id₂ . tactic.dec_trivial) :
   r{id₂ ↦ val₂}{id₁ ↦ val₁} = r{id₁ ↦ val₁}{id₂ ↦ val₂} :=
 begin
@@ -64,11 +64,11 @@ structure state : Type :=
 inductive instr : Type
 /- | add32 : (dst: nat) -> (src1:nat) -> (src2:nat) -> instr -/
 | add32 : nat -> nat -> nat -> instr
-// | mov32 : (dst:nat) -> (src:nat) -> instr
+/--| mov32 : (dst:nat) -> (src:nat) -> instr -/
 | mov32 : nat -> nat -> instr
 
 inductive code : Type 
 | Ins : instr -> code
 | Block : list code -> code
 
-end
+end otbn
